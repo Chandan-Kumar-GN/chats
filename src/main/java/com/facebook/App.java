323603d41a2aaa8fe32a56617d844bc013b72017
Chandan Kumar G N <Chandankumargn16@gmail.com>
2024-11-26 13:14:16 +0530

    Update App.java

--- src/main/java/com/facebook/App.java
+++ src/main/java/com/facebook/App.java
@@ -7,6 +7,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hi this is chandan");
-        System.out.println("Wellcome to Devops");
+        System.out.println("Wellcome to Devops")
     }
 }
