bf806180c95c1b04ba9f3570b720f1b849aa697c
Chandan Kumar G N <Chandankumargn16@gmail.com>
2024-11-26 12:57:48 +0530

    Update App.java

--- src/main/java/com/facebook/App.java
+++ src/main/java/com/facebook/App.java
@@ -6,6 +6,6 @@ package com.facebook;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hi this is chandan");
+        System.out.println("Hi this is chandan")
     }
 }
